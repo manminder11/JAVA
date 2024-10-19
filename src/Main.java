ff6cde6c5a54e1e32bed8868c0575335b587a8f5
Manminder <ManminderSingh@edu.sait.ca>
2024-10-18 19:32:09 -0600

    COMMENTING AND TESTIING

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.print("Hello world after the first time");
+
+
+        // this is considered as a comment single line
+
     }
 }
