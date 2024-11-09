0666cf44b179d605d503170ed479651a6b71d1f7
Manminder <ManminderSingh@edu.sait.ca>
2024-11-09 10:28:56 -0700

    working in a WAY

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.print("Hello world after the first time");
-
+        System.out.prinitln("Hello world after the second time");
+// ln is used to print the next line
+        // print is used to print the same line (without ln)
 
         // this is considered as a comment single line
         /*
