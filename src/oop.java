36cfcde8fef30bc2e37f10e10436ff79439d1cf3
Manminder <ManminderSingh@edu.sait.ca>
2024-12-02 16:33:37 -0700

    ooop

---
+++ src/oop.java
@@ -0,0 +1,22 @@
+public class oop {
+    public static void main(String[] args) {
+
+        // lets do the oop
+        // first will be the objects
+        // taking items from the oopcar file
+
+
+        // it make the by using the same name of the file in this case
+        // oopcar is that file which is declared somewhere else and called by using the oop concept as it made the oopcar object and then took the value from the same file
+        //
+        // SO NAMES CANNOT BE CHANGED HERE
+        oopcar myoop = new oopcar();
+
+        System.out.println(myoop.avg);
+        System.out.println(myoop.color);
+        myoop.available();
+        myoop.Condition();
+
+
+    }
+}
---
+++ src/oopcar.java
@@ -0,0 +1,16 @@
+public class oopcar {
+    String  name = "Lamborgini";
+    Integer model = 2020;
+    String color = "Yellow";
+    Double avg = 22.20;
+
+    void available() {
+        System.out.println("this car is available for customers ");
+    }
+
+    void Condition() {
+        System.out.println("Brand new almost");
+    }
+
+
+}
