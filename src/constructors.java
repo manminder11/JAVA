aa7ef8d82aa13b47c91ca4a0a9b051c5476db290
Manminder <ManminderSingh@edu.sait.ca>
2024-12-02 16:53:38 -0700

    ooop

---
+++ src/constructors.java
@@ -0,0 +1,11 @@
+public class constructors {
+    public static void  main(String[] args) {
+
+        Human human = new Human("Manminder", 20,50);
+
+        System.out.println(human.name);
+        System.out.println(human.weight);
+        System.out.println(human.age);
+
+    }
+}
